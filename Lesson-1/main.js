2df9343d0dda9183d75c58a87ca5d07ec5699203
OleksandrUntervald <untervaldthebest@gmail.com>
2024-11-04 18:36:05 +0200

    First HW

---
+++ Lesson-1/main.js
@@ -0,0 +1,77 @@
+/*************-- EX1 --*****************/
+
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 =  3.14;
+let number6 =  2.7;
+let number7 =  16;
+let bul1 = true;
+let bul2 = false;
+
+function worker (n){
+   console.log(n)
+}
+worker (str1)
+worker (str2)
+worker (str3)
+worker (str4)
+worker (number1)
+worker (number2)
+worker (number3)
+worker (number4)
+worker (number5)
+worker (number6)
+worker (number7)
+worker (bul1)
+worker (bul2)
+/*************-- EX1 --*****************/
+
+/*************-- EX2 --*****************/
+const firstName = 'Oleksandr ';
+const middleName = "Oleksandrovich ";
+const lastName = "Untervald";
+let fullName =  firstName + middleName + lastName ;
+
+console.log(fullName);
+
+/*************-- EX2 --*****************/
+
+
+/*************-- EX3 --*****************/
+
+let a = 100; let b = '100'; let c = true;
+function typeValue(n){
+    console.log(typeof n)
+}
+typeValue(a);
+typeValue(b);
+typeValue(c);
+
+/*************-- EX3 --*****************/
+
+
+
+
+/*************-- EX4 --*****************/
+
+let promptName = prompt("Enter: Oleksandr ");
+let promptMiddlename = prompt("Enter: Oleksandrovich ");
+let promptYears = prompt("Enter: 1994");
+
+
+
+console.log('name: ' + promptName );
+console.log('middleName: ' + promptMiddlename );
+console.log('years: ' + promptYears );
+
+
+/*************-- EX4 --*****************/
+
+
+
