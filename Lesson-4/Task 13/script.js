acad2ea54ae2cd4c6ca04b63bd2d8995c6c33a37
OleksandrUntervald <untervaldthebest@gmail.com>
2024-11-10 20:58:00 +0200

    fourth HW

--- Lesson-4/Task 13/script.js
+++ Lesson-4/Task 13/script.js
@@ -10,8 +10,8 @@ function exchange(sumUAH,currencyValues,exchangeCurrency){
             moneyBox = item;
         }
     }
-    let result = sumUAH/moneyBox.value;
-    return result;
+    let results = sumUAH/moneyBox.value;
+    return results;
 
 }
 console.log(exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'EUR'))
