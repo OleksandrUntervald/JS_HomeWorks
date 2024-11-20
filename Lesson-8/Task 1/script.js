2b03f767cdc36bebd151673538566f68f3469a15
OleksandrUntervald <untervaldthebest@gmail.com>
2024-11-20 20:35:25 +0200

    8th HW

--- Lesson-8/Task 1/script.js
+++ Lesson-8/Task 1/script.js
@@ -4,3 +4,36 @@
 //     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
 
 
+
+
+let originalUser = {
+    name: `Sergio`,
+    surname: `Onofriy`,
+    age: 22,
+    key4: function(){
+        console.log('Hello world')
+    }
+}
+
+
+
+function changer(arr){
+    if (arr){
+
+    let cloneFunction = [];
+        for (let key in arr){
+            if (typeof arr[key] === `function` ){
+               let cloner = arr[key].bind({});
+               cloneFunction.push({cloner, key})
+            }
+        }
+
+
+            let strChanger = JSON.stringify(arr);
+            let secondUser = JSON.parse(strChanger)
+            return secondUser
+    }
+
+}
+
+console.log(changer(originalUser))
--- Lesson-8/Task 2/script.js
+++ Lesson-8/Task 2/script.js
@@ -10,3 +10,18 @@
 // ];
 // за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
 //     Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+
+// ДОМАШКА СПИСАНА
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+coursesAndDurationArray.map((newObj, index) => ({...newObj, id:index + 1}))
+
